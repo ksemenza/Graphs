97ce80787b6d61569113b638f2ec67c9472b60bf
ksemenza <ksemenza13@gmail.com>
2020-08-13 11:59:36 -0400

    updated adv

--- projects/social/social.py
+++ projects/social/social.py
@@ -104,7 +104,7 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(100, 10)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
