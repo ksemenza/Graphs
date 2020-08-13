809a82b3c4c9fee74dcf2cf3fe073f984d4ff975
ksemenza <ksemenza13@gmail.com>
2020-08-13 09:28:39 -0400

    adv completed 975

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from collections import deque
 
 import random
 from ast import literal_eval
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -27,8 +28,117 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+
+class ExploreRooms:
+    def __init__(self, world: World):
+        self.world = world
+        self.start = self.world.starting_room
+        self.path = []
+
+#group utility function to class 
+#staticMethod() 
+    @ staticmethod
+    def get_neighbors(room: Room):
+        neighbors = []
+        if room.n_to is not None:
+            neighbors.append((room.n_to, 'n'))
+        if room.w_to is not None:
+            neighbors.append((room.w_to, 'w'))
+        if room.e_to is not None:
+            neighbors.append((room.e_to, 'e'))
+        if room.s_to is not None:
+            neighbors.append((room.s_to, 's'))
+        return neighbors
+
+    def DFT(self, room: Room):
+        # Depth first transversal return room with no unexplored pathways
+        path = [room]
+        stack = [path]
+        visited = set()
+
+        while stack:
+            # get path way and last room
+            path = stack.pop()
+            rm = path[-1][0]
+
+            #checks if it has been explored 
+            if rm.id not in visited:
+                visited.add(rm.id)
+
+                # If all of the neighboring rooms are on this path 
+                # this is a dead end 
+                # return the path to this room
+                
+                neighbors = self.get_neighbors(rm)
+                if all([neighbor_room.id in [path_room.id for path_room, _ in path]
+                        for neighbor_room, _ in neighbors]):
+                    # The start of this path is already a part of the main path.
+                    return path[1:]
+
+                # Otherwise, keep exploring this path.
+                for neighbor in neighbors:
+                    # Only add neighbors that aren't yet on the main path.
+                    if neighbor[0].id not in [room.id for room, _ in self.path]:
+                        new_path = [*path, neighbor]
+                        stack.append(new_path)
+        return path
+
+    def BFT(self, room: Room):
+        path = [room]
+        visited = set()
+        queue = deque()
+        queue.append(path)
+
+        while queue:
+            # Get a candidate path and the last room in it.
+            path = queue.popleft()
+            rm = path[-1][0]
+
+            # Check that this room hasn't been seen.
+            if rm.id not in visited:
+                visited.add(rm.id)
+
+                # If any neighbor of this room is not yet on the main path, return the path to this room.
+                neighbors = self.get_neighbors(rm)
+                if any([neighbor_room.id not in [path_room.id for path_room, _ in self.path]
+                        for neighbor_room, _ in neighbors]):
+                    # The start of this path is already a part of the main path.
+                    return path[1:]
+
+                # Otherwise, continue looking for a room with an unexplored neighbor.
+                for neighbor in neighbors:
+                    new_path = [*path, neighbor]
+                    queue.append(new_path)
+
+    def explore_paths(self):
+ 
+        print('Finding path...')
+        # Initialize path with a tuple (starting room, empty string)
+        self.path = [(self.start, '')] 
+
+        while True:  # 6.
+            # Create a new reference for this loop
+            main_path = self.path.copy()
+            # no explored paths
+            dft_path = self.DFT(main_path[-1])  
+            #append path to path list
+            main_path = [*main_path, *dft_path]  
+            # Update for BFT
+            self.path = main_path
+            bft_path = self.BFT(main_path[-1])  # 4.
+
+            # If BFT finds no unexplored rooms, the graph has been traversed.
+            if not bft_path:
+                return [direction for _, direction in self.path[1:]]  # 7.
+
+            main_path = [*main_path, *bft_path]  # 5.
+            # Update for next loop.
+            self.path = main_path
+
+
+mapper = ExploreRooms(world=world)
+traversal_path = mapper.explore_paths()
 
 
 # TRAVERSAL TEST
@@ -41,22 +151,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,6 @@
 import random
-from util import Queue, Stack
+from collections import deque
+
 
 class User:
     def __init__(self, name):
@@ -27,18 +28,18 @@ class SocialGraph:
         """
         Create a new user with a sequential integer ID
         """
-        self.last_id += 1  # automatically increment the ID to assign the new user
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
+        self.last_id += 1  # automatically increment the ID to assign the new user
 
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
-
+        
         Creates that number of users and a randomly distributed friendships
         between those users.
-
+        
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -47,72 +48,58 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
-        # Create friendships
-        
+        if num_users <= avg_friendships:
+            print("Warning! num_users must be greater than avg_friendships.")
+            return
+        # Add users.
         for i in range(num_users):
-            self.add_user(f'User {i}')
-            
-        # Generate all possible friendships
-        possible_friendships = []
-        
-        # Avoid dups by ensuring the first number is smaller than the second
-        for user_id in self.users:
-            for friend_id in range(user_id + 1, self.last_id + 1):
-                possible_friendships.append( (user_id, friend_id) )
-                
-        # shuffle all possible friendships
-        random.shuffle(possible_friendships)
-        
-        # create friendships for the first x pairs of the list
-        # x is determined by the formula: num_users * avg_friendships // 2
-        # need to divide by 2 since each add_friendship() creates 2 friendships
-        for i in range(num_users * avg_friendships // 2):
-            friendship = possible_friendships[i]
-            self.add_friendship(friendship[0], friendship[1])
-            
-        def populate_graph_2(self, num_users, avg_friendships):
-            # Reset graph
-            self.last_id = 0
-            self.users = {} # nodes
-            self.friendships = {} # edges
-
-        # add users
-        for i in range(num_users):
-            self.add_user(f'User {i}')
-
-        target_friendships = (num_users * avg_friendships) // 2
-        total_friendships = 0
-
-        while total_friendships < target_friendships:
-            user_id = random.randint(1, self.last_id)
-            friend_id = random.randint(1, self.last_id)
-            if self.add_friendship(user_id, friend_id):
-                total_friendships += 1
+            self.add_user(i)
+        # Friendships come in pairs.
+        friendships = (num_users * avg_friendships) // 2
+        for _ in range(friendships):
+        # Create unique, random friendships.
+            while True:
+                user = random.randint(0, self.last_id - 1)
+                friend = random.randint(0, self.last_id - 1)
+                # Be sure friendships aren't repeating.
+                if (friend != user
+                        and friend not in self.friendships[user]
+                        and user not in self.friendships[friend]):
+                    break
+            self.add_friendship(user, friend)
 
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
+        
         Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
-
+        extended visited with the shortest friendship path between them.
+        
         The key is the friend's ID and the value is the path.
         """
+  
         visited = {}  # Note that this is a dictionary, not a set
+ 
         # !!!! IMPLEMENT ME
-        q = Queue()
-        q.enqueue([user_id])
-
-        while q.size() > 0:
-            path = q.dequeue()
-            current_user = path[-1]
-
-            if current_user not in visited:
-                visited[current_user] = path
-                for friend in self.friendships[current_user]:
-                    q.enqueue(path + [friend])
-                return visited
+        # Start the path at the given id.
+        path = [user_id]
+        # Populate a queue with the starting path.
+        queue = deque([path])
+        while queue:
+            # Get a path from the queue.
+            path = queue.popleft()
+            # Get the last friend from the queue.
+            id_ = path[-1]
+            # Make sure they're not already in the visited.
+            if id_ not in visited:
+                # Add the friend and the path to the visited.
+                visited[id_] = path
+                # Find new friends to add.
+                for friend in self.friendships[id_]:
+                    # Add the new friend to the current path and add it to the queue.
+                    new_path = [*path, friend]
+                    queue.append(new_path)
+        return visited
 
 
 if __name__ == '__main__':
@@ -121,4 +108,3 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
-
